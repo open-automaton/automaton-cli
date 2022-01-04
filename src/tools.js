cb10946881053e30697ba5fc0363418a18d8ea94
Abbey Sparrow <abbey@khrome.net>
2022-01-03 21:05:26 -0700

    first commit

---
+++ src/tools.js
@@ -0,0 +1,15 @@
+const Automaton = require('@open-automaton/automaton');
+const CheerioEngine = require('@open-automaton/cheerio-mining-engine');
+const PuppeteerEngine = require('@open-automaton/puppeteer-mining-engine');
+
+module.exports = {
+    selectXpath : ()=>{
+
+    },
+    selectRegex : ()=>{
+
+    },
+    performScrape : ()=>{
+
+    }
+};
