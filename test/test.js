eec37e0501ca2728d38408aae9a682de49b7eb29
Abbey Sparrow <abbey@khrome.net>
2022-01-20 21:58:46 -0700

    argument test support

--- test/test.js
+++ test/test.js
@@ -2,6 +2,10 @@ const should = require('chai').should();
 const tool = require('../src/tools.js');
 const path = require('path');
 
+const hasArgument = (arg, str)=>{
+    return !!(str.match(new RegExp('\-\-'+arg)));
+}
+
 describe('automaton-cli', ()=>{
     describe('cli-tool', ()=>{
         it('selects xpath', ()=>{
@@ -12,7 +16,6 @@ describe('automaton-cli', ()=>{
                 </div>
             `, (err, selection)=>{
                 should.not.exist(err);
-                console.log(selection);
             });
         });
 
@@ -34,6 +37,16 @@ describe('automaton-cli', ()=>{
                 path.join(
                     __dirname, '..', 'bin', 'auto'
                 )+' --help', (err, output)=>{
+                    hasArgument('help', output).should.equal(true);
+                    hasArgument('version', output).should.equal(true);
+                    hasArgument('data', output).should.equal(true);
+                    hasArgument('puppeteer-chromium', output).should.equal(true);
+                    hasArgument('playwright-chromium', output).should.equal(true);
+                    hasArgument('playwright-webkit', output).should.equal(true);
+                    hasArgument('playwright-firefox', output).should.equal(true);
+                    hasArgument('cheerio', output).should.equal(true);
+                    hasArgument('jsdom', output).should.equal(true);
+                    hasArgument('a-missing-flag', output).should.equal(false);
                     done();
                 }
             );
